2832ab5ea7093b9c22fcd8c0267f5d5c68f0a986
Jonatan Claros <jona.cc.93@gmail.com>
2022-07-21 12:29:00 -0300

    primero

---
+++ ultimointentovitetest/tests/components/Hola.test.tsx
@@ -0,0 +1,15 @@
+import Hola from '../../src/components/Hola';
+import { render ,screen} from '@testing-library/react';
+import React from 'react';
+
+
+describe('Hola', () => {
+
+
+    it('should render correctly', () => {
+        const { container } = render(<Hola />);
+        expect(container.firstChild).toMatchSnapshot();
+        screen.debug() 
+    }
+    );
+})
